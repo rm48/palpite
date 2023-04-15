463292d9dbb7ee6adca3202a49e8bf57ed8583f9
rei <reinaldo58972@gmail.com>
2023-04-15 12:04:56 -0300

    menu 50 telas
    arquivos html, css e 2 js

---
+++ js/telas.js
@@ -0,0 +1,157 @@
+function mudarTela(){
+			switch (tela){		
+				case 1:			
+				document.body.style.backgroundImage = "url('https://64.media.tumblr.com/680a8ecc1434638c7372c895b425f98b/ea1c59ab2242365e-5b/s2048x3072/378cbd9cd5abb0ce742002568279e3e4ff3e48d9.jpg')";
+				break;	
+				case 2:
+				document.body.style.backgroundImage="url('https://64.media.tumblr.com/5d8979c6daf4688dce6f4213b75fa37f/debb76131f5117af-3c/s1280x1920/efea0f4f9aababef184c13005b23e16c0c57c1a1.jpg')";
+				break;
+				case 3:
+				document.body.style.backgroundImage="url('https://64.media.tumblr.com/deee4d0dc88b3f12cc9c55b2bc7a2707/ea1c59ab2242365e-4c/s2048x3072/fd9f57e0c92412c3263e5f6647accf5a819dc30e.jpg')";
+				break;
+				case 4:
+				document.body.style.backgroundImage="url('https://64.media.tumblr.com/db55d2353fb4232881b6fe5c21a86c4b/9d5d1790477ef735-a4/s2048x3072/6843e5b54257a277e49064cd88127cdc62c27ceb.jpg')";		
+				break;
+				case 5:
+				document.body.style.backgroundImage="url('https://64.media.tumblr.com/b75c2a880f89ac1fcdccca103f84d2de/9d5d1790477ef735-21/s2048x3072/dc285103ff72d8c710b15e66b07e99cda5cfb2b8.jpg')";
+				break;
+				case 6:	
+				document.body.style.backgroundImage="url('https://64.media.tumblr.com/0156ac261025e2593d69e2375043bee8/25afc58b3fe56de9-da/s1280x1920/5e2fd66e1cb39c871c8a27753cbf94706555498e.jpg')";
+				break;
+				case 7:
+				document.body.style.backgroundImage="url('https://64.media.tumblr.com/3ddb53a1dadafc19c6efcc6fc4438368/0a93e674ca89c780-2c/s1280x1920/4b11a039ffaf3139fc2b104247a502d0092ceca6.jpg')";
+				break;
+				case 8:
+				document.body.style.backgroundImage="url('https://64.media.tumblr.com/903daecf79203546792d287e0fb1d595/tumblr_ozoosyF2Ul1rel9soo1_540.jpg')";
+				break;
+				case 9:
+				document.body.style.backgroundImage="url('https://64.media.tumblr.com/8d415a0281c2824bd9d9706c14554b96/tumblr_od9e2qwEIe1rel9soo1_540.jpg')";
+				break;
+				case 10:
+				document.body.style.backgroundImage="url('https://64.media.tumblr.com/e0907bbb79dec11f5cbe8df61f5f98a9/75fe5a25fc619166-0d/s1280x1920/3aa738d565bdd7c80f4a57c490b61979fa562f4f.pnj')";
+				break;
+				case 11:
+				document.body.style.backgroundImage="url('https://64.media.tumblr.com/0a70d8b8cecc1baf63d3484f8b50bbc1/tumblr_phpdzvzAON1rel9soo1_1280.jpg')";
+				break;
+				case 12:
+				document.body.style.backgroundImage="url('https://64.media.tumblr.com/cd527711c0b14b00ea87b91b74e00f67/862b03318142c00e-4b/s1280x1920/4bcf1b215ad029907bd6b5eec79d8da3cb2e1f26.jpg')";
+				break;
+				case 13:
+				document.body.style.backgroundImage="url('https://64.media.tumblr.com/31027fe2bfee8a1824e7555a9971597c/d748aac14a957420-bf/s1280x1920/9f5832a6b00254526ad73ca5dae2d973c65b70ca.jpg')";
+				break;
+				case 14:
+				document.body.style.backgroundImage="url('https://64.media.tumblr.com/3a71433c3f4562f8deaefacb7a762f26/a464850cb9361efc-cd/s540x810/5ed60f4e8064623a833c6e73f63c66be77fb50b0.jpg')";
+				break;
+				case 15:
+				document.body.style.backgroundImage="url('https://64.media.tumblr.com/4a0326ab2974950c074255021190a772/97f37a73727bd9d5-e0/s1280x1920/5dc78f022e16566d8f04d67a7444ac64521bc8d3.jpg')";
+				break;
+				case 16:
+				document.body.style.backgroundImage="url('https://64.media.tumblr.com/1e51f87e7d8a723766fd12a43fd0c843/33a1eca73bcb47e6-16/s2048x3072/0a565457b26c85148f9fee2b5ea1166c10f20974.jpg')";
+				break;
+				case 17:
+				document.body.style.backgroundImage="url('https://64.media.tumblr.com/7dbf57f4afd9f9a6b6e193f517a741ac/ee04a7c807f0ebc7-79/s2048x3072/4b9fe553c4ae50813514b02946d8e83c625bc128.jpg')";
+				break;
+				case 18:
+				document.body.style.backgroundImage="url('https://64.media.tumblr.com/e812f4695ffcd7078061f40ecf99d318/25afc58b3fe56de9-44/s1280x1920/7d13c5005f84426d6454a37274c4447a39602143.jpg')";
+				break;
+				case 19:
+				document.body.style.backgroundImage="url('https://64.media.tumblr.com/8d0b69e84d3abf7bff33987b9fb00e3c/25afc58b3fe56de9-b5/s1280x1920/d2108c482e762927d16e23868f437fc9537f4b79.jpg')";	
+				break;
+				case 20:
+				document.body.style.backgroundImage="url('https://64.media.tumblr.com/7f1c28c6b76c1077fad1edc719fe2e64/25afc58b3fe56de9-84/s1280x1920/f60866b5636ecbe3733a4a99e9a34bcd918c6b53.jpg')";
+				break;
+				case 21:
+				document.body.style.backgroundImage="url('https://64.media.tumblr.com/fd8d060c701096aed3ec2591a0be7e19/tumblr_pa4u2xFk8S1rel9soo1_1280.jpg')";
+				break;
+				case 22:
+				document.body.style.backgroundImage="url('https://64.media.tumblr.com/6bfba0e470ce2e1f46aa1cc81a46bf51/5a267a918fa06013-4c/s2048x3072/22c78e47b2a9c18c25ab9a9930e5c5e4fb1f46bf.pnj')";
+				break;
+				case 23:
+				document.body.style.backgroundImage="url('https://64.media.tumblr.com/fdaf8b7c16692cd62097438b6ef5afa1/26cf23a30e6e24ae-1c/s1280x1920/1675320243796f7a8c8086867abaa36022b61ee4.pnj')";
+				break;
+				case 24:
+				document.body.style.backgroundImage="url('https://p2.trrsf.com/image/fget/cf/940/0/images.terra.com/2021/12/24/771177070-legion-of-the-super-heroes.jpg')";
+				break;
+				case 25:
+				document.body.style.backgroundImage="url('https://64.media.tumblr.com/6700db896a515a4671bfdeb35086c715/75fe5a25fc619166-10/s1280x1920/bed5e2760e1361a1c2aa56f870a8eb2746f3aac5.pnj')";
+				break;
+				case 26:
+				document.body.style.backgroundImage="url('https://64.media.tumblr.com/433296d3233c7fc5b0272f9e15519f08/e632e6999ba58a61-e7/s2048x3072/45f709d87921707ce6e9b32e0f784a3ecbfbc3ef.pnj')";
+				break;
+				case 27:
+				document.body.style.backgroundImage="url('https://i.pinimg.com/236x/ac/73/e7/ac73e759592bb4a4a17543f3f1fa62d8.jpg')";
+				break;
+				case 28:
+				document.body.style.backgroundImage="url('https://64.media.tumblr.com/243d91aad3f8010855919b9351d29008/0b9c31221eadfa58-7b/s2048x3072/35633b6e1e4f90daaf9e48328a8013e49b9d217f.pnj')";				
+				break;
+				case 29:
+				document.body.style.backgroundImage="url('https://i.pinimg.com/originals/44/78/3d/44783dfc2450cb18ac733a46b981f4f3.jpg')";
+				break;
+				case 30:
+				document.body.style.backgroundImage="url('https://64.media.tumblr.com/aeb292da06522d44d895c76ee7128a7a/947afc5867c7de89-82/s2048x3072/31c038db8fb6422b4d3f61f73bee60f1312b80a8.jpg')";
+				
+				break;
+				case 31:
+				document.body.style.backgroundImage="url('https://i.pinimg.com/236x/ba/81/c8/ba81c857479a33ef2fc392153cd02b9e.jpg')";
+				break;
+				case 32:
+				document.body.style.backgroundImage="url('https://i.pinimg.com/236x/bd/23/ce/bd23ce415dc8f0e15e8a7ad9aeef1371.jpg')";
+				break;
+				case 33:
+				document.body.style.backgroundImage="url('https://i.pinimg.com/236x/d4/c5/85/d4c585e0c41a4325650660b128f77d11.jpg')";
+				break;
+				case 34:
+				document.body.style.backgroundImage="url('https://i.pinimg.com/474x/20/5d/1f/205d1f484ddd115559955668e5c93d63.jpg')";
+				break;
+				case 35:
+				document.body.style.backgroundImage="url('https://i.pinimg.com/736x/d3/35/14/d335142566cc77a2955f9a6b0b22c48a.jpg')";
+				break;
+				case 36:
+				document.body.style.backgroundImage="url('https://i.pinimg.com/736x/5e/c3/c9/5ec3c917ab57ec6a94399d6f44a51fd2--shop-justice-justice-league.jpg')";
+				break;
+				case 37:
+				document.body.style.backgroundImage="url('https://i.pinimg.com/originals/e6/86/68/e686682c8cb87b0b1b55492a21d41af0.jpg')";
+				break;
+				case 38:
+				document.body.style.backgroundImage="url('https://i.pinimg.com/originals/36/67/92/3667923678d95066e55c7f8f43d877ee.jpg')";
+				break;
+				case 39:
+				document.body.style.backgroundImage="url('https://i.pinimg.com/236x/a9/9a/2a/a99a2a91552673281ce482424e87a340--wonder-woman-cover-art.jpg')";
+				break;
+				case 40:
+				document.body.style.backgroundImage="url('https://i.pinimg.com/originals/25/df/43/25df43753c19bfa45e677034620b04d6.jpg')";
+				break;
+				case 41:
+				document.body.style.backgroundImage="url('https://stories.recreio.com.br/quem-foi-o-primeiro-super-heroi-da-marvel-comics/assets/4.gif')";
+				break;
+				case 42:
+				document.body.style.backgroundImage="url('https://opastelnerd.files.wordpress.com/2015/07/91lrbkksmpl.jpg')";
+				break;
+				case 43:
+				document.body.style.backgroundImage="url('https://i.pinimg.com/474x/80/9d/8a/809d8ab8df02cc598adc747f8ba2028b.jpg')";
+				break;
+				case 44:
+				document.body.style.backgroundImage="url('https://www.fatosdesconhecidos.com.br/wp-content/uploads/2017/02/armas-4.jpg')";
+				break;
+				case 45:
+				document.body.style.backgroundImage="url('https://i.pinimg.com/originals/e5/f9/70/e5f97050dc2ad9becac0b94aa1674d1e.jpg')";
+				break;
+				case 46:
+				document.body.style.backgroundImage="url('https://i.pinimg.com/550x/05/d5/e7/05d5e7a0dfd7f37c9124e2196ea053c6.jpg')";
+				break;
+				case 47:
+				document.body.style.backgroundImage="url('https://i.pinimg.com/originals/d9/e6/9c/d9e69c76e2ad733d45c2c5d29b02cad6.jpg')";
+				break;
+				case 48:
+				document.body.style.backgroundImage="url('https://static.wikia.nocookie.net/marveldatabase/images/4/46/Ms._Marvel_Vol_2_5.jpg/revision/latest?cb=20180109020202')";
+				break;
+				case 49:
+				document.body.style.backgroundImage="url('https://static.wikia.nocookie.net/marveldatabase/images/b/bf/Ms._Marvel_Vol_2_3.jpg/revision/latest?cb=20180109020022')";
+				break;
+				case 50:
+				document.body.style.backgroundImage="url('https://i.pinimg.com/originals/9c/d8/c8/9cd8c8e36b80b6a053d2ec959bfdba33.jpg')";
+				break;
+				default:
+				document.body.style.backgroundImage = "url(./img/0.jpg)";
+			}
+		}
